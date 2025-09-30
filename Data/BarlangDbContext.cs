e266c245050b73434c495750c75e9db198a38155
Tóth Árpád <toth.arpad@burg.lo.hu>
2025-09-30 08:46:34 +0200

    DbContext created

--- Data/BarlangDbContext.cs
+++ Data/BarlangDbContext.cs
@@ -1,7 +1,14 @@
-﻿namespace Barlangok.Data
-{
-    public class BarlangDbContext
-    {
+﻿using Barlangok.Models;
+using Microsoft.EntityFrameworkCore;
 
+namespace Barlangok.Data
+{
+    public class BarlangDbContext : DbContext
+    {
+        public BarlangDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BarlangModel> Barlangok {  get; set; }
     }
 }
